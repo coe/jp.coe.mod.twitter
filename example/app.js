fcd19123c8c05159b98ce70673696303cac7b1f6
coe <coe.app@gmail.com>
2014-10-03 15:44:20 +0900

    コミット

--- example/app.js
+++ example/app.js
@@ -1,5 +1,5 @@
 
-var twitter = require('twitter').Twitter({
+var twitter = require('jp.coe.mod.twitter').Twitter({
       consumerKey:config.twitter.consumerKey,
       consumerSecret:config.twitter.consumerSecret,
       accessTokenKey: Ti.App.Properties.getString('twitterAccessTokenKey', ''),
